4173998dc3914cb6c29e562c49855fcb8d04fd60
Wangwd233 <88479346+Wangwd233@users.noreply.github.com>
2022-06-08 02:33:46 +1000

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CrowdFundingContract.Models;
+
+namespace CrowdFundingContract.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<CrowdFundingContract.Models.RequestFunds> RequestFunds { get; set; }
+        public DbSet<CrowdFundingContract.Models.Funders> Funders { get; set; }
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+
+namespace CrowdFundingContract.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string AccountAddress { get; set; }
+        public string Key { get; set; }
+    }
+}
