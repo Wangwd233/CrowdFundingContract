354193022e1b20ae9ad6b6521e8728879b8e042c
Wangwd233 <88479346+Wangwd233@users.noreply.github.com>
2022-06-08 02:29:04 +1000

    Add files via upload

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CrowdFundingContract.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CrowdFundingContract.Areas.Identity.IdentityHostingStartup))]
+namespace CrowdFundingContract.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
